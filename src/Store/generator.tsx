0b2354a3066e86b1c0b701ae2c966aaa5aaa0ffd
Alicja Lysien <61288677+AlicjaLysien@users.noreply.github.com>
2023-12-30 21:40:39 +0100

    Added structure to chinese generator

--- src/Store/generator.tsx
+++ src/Store/generator.tsx
@@ -2,16 +2,19 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import Generator from "../Types/Generator";
 import Parts from "../Types/Parts";
 import Genre from "../Types/Genre";
+import chinese from "../Types/chinese";
 import fantasy from "../Types/fantasy";
 import japanese from "../Types/japanese";
 import {ArrayWithNames} from "../Types/Name";
 
 const initialState: Generator = {
 	generatedNames: {
+		chinese: [],
 		fantasy: [],
 		japanese: []
 	},
 	parts: {
+		chinese,
 		fantasy,
 		japanese
 	},
@@ -25,12 +28,15 @@ export const slice = createSlice({
 		generateNames: (state: Generator, action: PayloadAction<{ genre: Genre }>) => {
 			for(let i = 0; i < state.numberOfNames; i++){
 				const numberOfParts: number = Math.floor(Math.random() * (2) + 2);
-				const usedParts: Parts = (action.payload.genre === "fantasy" && state.parts.fantasy) || (state.parts.japanese);
+				const usedParts: Parts = 
+					(action.payload.genre === "chinese" && state.parts.chinese) ||
+					(action.payload.genre === "fantasy" && state.parts.fantasy) ||
+					(state.parts.japanese);
 				const partFirst: string = usedParts[0][Math.floor(Math.random() * (usedParts[0].length - 1))];
 				const partSecond: string = usedParts[1][Math.floor(Math.random() * (usedParts[1].length - 1))];
 				let createdName: string =  partFirst.charAt(0).toUpperCase() + partFirst.slice(1) + partSecond;
 				if(numberOfParts === 3){
-					if(action.payload.genre === "fantasy") {
+					if(action.payload.genre === ("fantasy" || "chinese")) {
 						const partThird: string = usedParts[2][Math.floor(Math.random() * (usedParts[2].length - 1))];
 						createdName = createdName + partThird;
 					}
@@ -39,6 +45,7 @@ export const slice = createSlice({
 						createdName = createdName + partThird;
 					}
 				}
+				if(action.payload.genre === "chinese") state.generatedNames.chinese[i] = createdName;
 				if(action.payload.genre === "fantasy") state.generatedNames.fantasy[i] = createdName;
 				if(action.payload.genre === "japanese") state.generatedNames.japanese[i] = createdName;
 			}
@@ -52,6 +59,7 @@ type State = {
     generator: Generator
 }
 
+export const generatedNamesChinese = (state: State): [] | ArrayWithNames => state.generator.generatedNames.chinese;
 export const generatedNamesFantasy = (state: State): [] | ArrayWithNames => state.generator.generatedNames.fantasy;
 export const generatedNamesJapanese = (state: State): [] | ArrayWithNames => state.generator.generatedNames.japanese;
 export const numberOfNames = (state: State): number => state.generator.numberOfNames;
