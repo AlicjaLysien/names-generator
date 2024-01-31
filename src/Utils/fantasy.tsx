b81b03f67d9261bf8287996ba6f3fb5e48cf701e
Alicja Lysien <61288677+AlicjaLysien@users.noreply.github.com>
2024-01-31 12:57:10 +0100

    Fantasy utils -> parts added

--- src/Utils/fantasy.tsx
+++ src/Utils/fantasy.tsx
@@ -4,16 +4,22 @@ const fantasy: Parts = [
 	[
 		"a", "o", "e", "i", "u", "ai", "ya", "ei", "el", "la", "li", "io", "yo", "go", "zo",
 		"fa", "fi", "fo", "lio", "fe", "fa", "fio", "tha", "tho", "gra", "tio", "pa", "lio", 
-		"lie", "vio", "via", "ve", "ke", "ko", "co", "bo", "se", "se"
+		"lie", "vio", "via", "ve", "ke", "ko", "co", "bo", "se", "se", "mal", "mol", "van",
+		"kla", "al", "el", "va", "gre", "ska", "ru", "aks", "ax", "ex", "xe", "xa", "xia",
+		"xo", "xy", "lu", "ley", "loy", "muo", "fou", "sha", "y", "pa", "fa", "fei", "fyo",
+		"fi", "fia", "ge", "gra", "gio"
 	],
 	[
 		"ma", "to", "ye", "zi", "uo", "kai", "wa", "yo", "la", "lo", "fi", "fa", "wu", "vio",
 		"via", "ro", "ra", "re", "ri", "tho", "tha", "jo", "xo", "xa", "xe", "tu", "su", "ze",
-		"ba", "pi", "ti", "ho", "ha", "da" ,"dio"
+		"ba", "pi", "ti", "ho", "ha", "da" ,"dio", "the", "xio", "fu", "va", "ve", "mio", "mi"
 	],
 	[
 		"zor", "yo", "ya", "lion", "dor", "dian", "no", "ria", "lia", "dion", "cia", "sia",
-		"vov", "nov", "hon", "thas", "jon", "zon", "nor", "na", "rey", "ray", "xis", "nox"
+		"vov", "nov", "hon", "thas", "jon", "zon", "nor", "na", "rey", "ray", "xis", "nox",
+		"rion", "ze", "nas", "ry", "rio", "sa", "re", "rosh", "rim", "non", "rol", "ol",
+		"ce", "ly", "ha", "her", "tu", "mi", "has", "yev", "dan", "dian", "dien", "ka", "kin",
+		"zul", "zol", "par", "roth", "ty", "su", "hi", "ris"
 	]
 ];
 
