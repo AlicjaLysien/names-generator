f51fa22d6e6d9b8286a107c0b7006e4ba05af05d
Alicja Lysien <61288677+AlicjaLysien@users.noreply.github.com>
2024-01-31 10:11:23 +0100

    Reducer for favourite names
    (cherry picked from commit 2878704122fdece54fb7a68f52b0f88f78c28099)

--- src/Store/configureStore.tsx
+++ src/Store/configureStore.tsx
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import generatorReducer from "./generator";
+import generatorFavourite from "./favourite";
 
 export default configureStore({
 	reducer: {
 		generator: generatorReducer,
+		favourite: generatorFavourite,
 	}
 });
