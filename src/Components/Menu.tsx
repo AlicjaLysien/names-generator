ee7aad0637286755f9dffa3919feae9b2773a2d5
Alicja Lysien <61288677+AlicjaLysien@users.noreply.github.com>
2024-01-31 09:07:22 +0100

    Type Genre and array genres functionality rewritten
    (cherry picked from commit c347612182099572d06a366970858d7594a2b9d4)

--- src/Components/Menu.tsx
+++ src/Components/Menu.tsx
@@ -1,18 +1,15 @@
 import {NavLink} from "react-router-dom";
+import {genres} from "../Types/Genre";
 import "../Styles/Menu.scss";
 
 function Menu() {
 	return (
 		<div className="Menu">
-			<NavLink to="/generator/fantasy">
-				fantasy
-			</NavLink>
-			<NavLink to="/generator/japanese">
-				japanese
-			</NavLink>
-			<NavLink to="/generator/chinese">
-				chinese
-			</NavLink>
+			{genres.map((genre, index) =>
+				<NavLink key={`${genre}-${index}`} to={`/generator/${genre}`}>
+					{genre}
+				</NavLink>)
+			}
 		</div>
 	);
 }
